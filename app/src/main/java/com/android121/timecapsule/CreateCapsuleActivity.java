f62d2880c68adeac4765cfa82a493884d5989eaf
Kyle <kyledlee@yahoo.com>
2019-05-01 16:35:02 -0700

    Added Invitation class for db. Invite friends button now adds to invitation table. Need to error check the friend address and clear.

--- app/src/main/java/com/android121/timecapsule/CreateCapsuleActivity.java
+++ app/src/main/java/com/android121/timecapsule/CreateCapsuleActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -22,6 +23,7 @@ public class CreateCapsuleActivity extends AppCompatActivity {
     Button mCreateCapsuleButton;
     EditText mRecipientEditText;
     EditText mOpenDateEditText;
+    EditText mInviteFriendEditText;
     FirebaseFirestore db;
 
     @Override
@@ -35,13 +37,39 @@ public class CreateCapsuleActivity extends AppCompatActivity {
         mCreateCapsuleButton = (Button) findViewById(R.id.button_create_capsule);
         mRecipientEditText = (EditText) findViewById(R.id.edit_text_recipient);
         mOpenDateEditText = (EditText) findViewById(R.id.edit_text_open_date);
+        mInviteFriendEditText = (EditText) findViewById(R.id.edit_text_invite_address);
 
         db = FirebaseFirestore.getInstance();
 
     }
 
     public void inviteFriends(View v){
-        // Go to new inviteFriends activity???
+        // Create Document to enter into database
+
+        String userId = mInviteFriendEditText.getText().toString();
+
+        Invitation invitation = new Invitation(null, userId);
+
+        // Insert document into contributions table
+        db.collection("invitations")
+                .add(invitation)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+        // Show toast message to confirm submission
+        String inviteToastString = userId + " invited!";
+        Toast noteSubmittedToast = new Toast(this);
+        noteSubmittedToast.makeText(this, inviteToastString, Toast.LENGTH_SHORT).show();
     }
 
     public void createCapsule(View v){
---
+++ app/src/main/java/com/android121/timecapsule/Invitation.java
@@ -0,0 +1,11 @@
+package com.android121.timecapsule;
+
+public class Invitation {
+    String mCapsuleId;
+    String mUserId;
+
+    Invitation(String capsuleId, String userId) {
+        this.mCapsuleId = capsuleId;
+        this.mUserId = userId;
+    }
+}
