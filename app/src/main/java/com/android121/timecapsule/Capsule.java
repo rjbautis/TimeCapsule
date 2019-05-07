383aefcda4b1cd893e34d36c12747439ba4463b2
Ryan Bautista <rjbautis1@gmail.com>
2019-05-07 14:56:08 -0700

    * Fixes up issues in build.gradle * Renames ID's in EditItem xml and class

--- app/src/main/java/com/android121/timecapsule/Capsule.java
+++ app/src/main/java/com/android121/timecapsule/Capsule.java
@@ -4,10 +4,14 @@ import java.util.Date;
 
 class Capsule {
     Date mDateCreated;    // Change to date data type?
-    String mOpenDate;       // Change to date data type?
+    Date mOpenDate;       // Change to date data type?
     String mRecipientId;
 
-    Capsule(Date dateCreated, String openDate, String recipientId) {
+    // Empty constructor needed to convert Firestore document objects back to capsule objects
+    Capsule() {
+    }
+
+    Capsule(Date dateCreated, Date openDate, String recipientId) {
         this.mDateCreated = dateCreated;
         this.mOpenDate = openDate;
         this.mRecipientId = recipientId;
