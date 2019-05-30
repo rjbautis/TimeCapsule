631da51662ca16d9ac6b2a7bd0b181a8146c85b2
Kyle <kyledlee@yahoo.com>
2019-05-30 01:10:29 -0700

    Updated OpenCapsuleActivity to use CarouselView

--- app/src/main/java/com/android121/timecapsule/OpenCapsuleActivity.java
+++ app/src/main/java/com/android121/timecapsule/OpenCapsuleActivity.java
@@ -2,13 +2,22 @@ package com.android121.timecapsule;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.MediaController;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,6 +26,8 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ViewListener;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -27,21 +38,34 @@ import java.util.List;
 
 public class OpenCapsuleActivity extends AppCompatActivity {
 
-    private static final String TAG = OpenCapsuleActivity.class.getSimpleName();
-    String mCapsuleId;
+
+    private static final String TAG = testCustomCarouselActivity.class.getSimpleName();
+    CarouselView customCarouselView;
+    int NUMBER_OF_PAGES = 5;
     private FirebaseFirestore db;
-    String capsuleName;
-    List<ContributionItem> contributionItemList = new ArrayList<ContributionItem>();
-    ContributionAdapter adapter;
-    Context context;
-    ListView listView;
+
+    String mCapsuleId;
+    String content;
+    List<ContributionItem> contributionList = new ArrayList<ContributionItem>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        //mCapsuleId = "dvaOmVVVbVaHXjcjn2AI";
+        //mCapsuleId = "PoiSYfSgGABQyuw4l9wH";
+        //mCapsuleId = "3csUst4QZOjsrLDupeGT";
+
+        final Context context = OpenCapsuleActivity.this;
+
+
+        db = FirebaseFirestore.getInstance();
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_capsule);
-
-        listView = findViewById(R.id.list_view_contributions);
 
         // Get capsule id from bundle
         Bundle extras = getIntent().getExtras();
@@ -51,44 +75,104 @@ public class OpenCapsuleActivity extends AppCompatActivity {
             Log.d(TAG, "capsuleId received from bundle:" + mCapsuleId);
         }
 
-        //mCapsuleId = "bdWcQNkGbvMSSVpS1fad";
-
-        db = FirebaseFirestore.getInstance();
-
         Query findContributionsQuery = db.collection("contributions").whereEqualTo("capsuleId", mCapsuleId);
-
         findContributionsQuery.get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                    @Override
-                    public void onSuccess(QuerySnapshot documentSnapshots) {
-                        // ...
+                                          @Override
+                                          public void onSuccess(QuerySnapshot documentSnapshots) {
+                                              // ...
 
-                        // Get the last visible document
-                        if(documentSnapshots.size() == 0){
-                            Log.d(TAG, "Query returned 0 results");
-                            String contributionsNotFoundString = "No contributions in this capsule.";
-                            Toast contributionsNotFoundToast = new Toast(OpenCapsuleActivity.this);
-                            contributionsNotFoundToast.makeText(OpenCapsuleActivity.this, contributionsNotFoundString, Toast.LENGTH_SHORT).show();
+                                              // Get the last visible document
+                                              if (documentSnapshots.size() == 0) {
+                                                  Log.d(TAG, "Query returned 0 results");
+                                                  String contributionsNotFoundString = "No contributions in this capsule.";
+                                                  Toast contributionsNotFoundToast = new Toast(OpenCapsuleActivity.this);
+                                                  contributionsNotFoundToast.makeText(OpenCapsuleActivity.this, contributionsNotFoundString, Toast.LENGTH_SHORT).show();
 
-                            return;
-                        }
+                                                  return;
+                                              }
 
-                        Log.d(TAG, "Query returned : " + documentSnapshots.size());
-                        for(int i = 0; i < documentSnapshots.size(); i++){
-                            DocumentSnapshot contributionDocument = documentSnapshots.getDocuments().get(i);
-                            Log.d(TAG, "adding contribution: type: " + contributionDocument.getString("type") + ", content: " + contributionDocument.getString("content"));
-                            ContributionItem contribution = new ContributionItem(contributionDocument.getString("type"), contributionDocument.getString("content"), contributionDocument.getString("userId"), contributionDocument.getString("name"));
-                            contributionItemList.add(contribution);
+                                              Log.d(TAG, "Query returned : " + documentSnapshots.size());
+                                              for (int i = 0; i < documentSnapshots.size(); i++) {
+                                                  DocumentSnapshot contributionDocument = documentSnapshots.getDocuments().get(i);
+                                                  Log.d(TAG, "adding contribution: type: " + contributionDocument.getString("type") + ", content: " + contributionDocument.getString("content"));
+                                                  ContributionItem contribution = new ContributionItem(contributionDocument.getString("type"), contributionDocument.getString("content"), contributionDocument.getString("userId"), contributionDocument.getString("name"));
+                                                  contributionList.add(contribution);
 
-                        }
-
-                        adapter = new ContributionAdapter(OpenCapsuleActivity.this, contributionItemList);
-                        listView.setAdapter(adapter);
+                                              }
 
 
+                                              customCarouselView = (CarouselView) findViewById(R.id.carouselView);
 
-                    }
-                });
+                                              // set ViewListener for custom view
 
+
+
+                                              ViewListener viewListener = new ViewListener() {
+
+                                                  @Override
+                                                  public View setViewForPosition(int position) {
+                                                      Log.d(TAG, "POSITION = " + position);
+
+                                                      View customView = getLayoutInflater().inflate(R.layout.test_custom_carousel_text_view, null);
+                                                      //set view attributes here
+
+                                                      RelativeLayout.LayoutParams params= new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                                                      //RelativeLayout relView = findViewById(R.id.carousel_relative_view);
+
+
+                                                      TextView textView = customView.findViewById(R.id.carousel_text_view);
+                                                      ImageView imageView = customView.findViewById(R.id.carousel_image_view);
+                                                      VideoView videoView = customView.findViewById(R.id.carousel_video_view);
+                                                      TextView donorView = customView.findViewById(R.id.carousel_donor_view);
+                                                      if(position < contributionList.size()) {
+                                                          ContributionItem currentContribution = contributionList.get(position);
+                                                          if(currentContribution.type.equals("text")){
+                                                              textView.setText(currentContribution.content);
+                                                              textView.setVisibility(TextView.VISIBLE);
+
+                                                              params.addRule(RelativeLayout.BELOW, R.id.carousel_text_view);
+                                                          } else if (currentContribution.type.equals("photo")){
+                                                              Glide.with(context).load(currentContribution.content).into(imageView);
+                                                              imageView.setVisibility(TextView.VISIBLE);
+                                                              params.addRule(RelativeLayout.BELOW, R.id.carousel_image_view);
+                                                          } else if (currentContribution.type.equals("video")){
+                                                              Uri uri=Uri.parse(currentContribution.content);
+                                                              videoView.setVideoURI(uri);
+                                                              videoView.setVisibility(VideoView.VISIBLE);
+                                                              //videoView.start();
+
+                                                              MediaController mediaController = new MediaController(context);
+                                                              videoView.setMediaController(mediaController);
+                                                              mediaController.setAnchorView(videoView);
+
+                                                              params.addRule(RelativeLayout.BELOW, R.id.carousel_video_view);
+                                                          }
+
+
+
+
+                                                          String donorString = "Contributed by: " + currentContribution.userName;
+                                                          donorView.setText(donorString);
+
+
+                                                          //donorView.setLayoutParams(params);
+
+                                                          //relView.setGravity(Gravity.CENTER);
+
+                                                      }
+
+                                                      return customView;
+                                                  }
+
+
+                                              };
+
+                                              customCarouselView.setViewListener(viewListener);
+                                              customCarouselView.setPageCount(contributionList.size());
+                                          }
+                                      }
+                );
     }
 }
+
